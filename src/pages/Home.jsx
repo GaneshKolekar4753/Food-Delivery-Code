cefcb1b10d2ab5cc2b08439ea09a4010c3414a07
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2023-12-23 10:13:11 +0530

    added routes and navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+  <>
+   <Navbar/>
+   <div>Home</div>
+   <Footer/>
+  </>
+  )
+}
+
+export default Home
